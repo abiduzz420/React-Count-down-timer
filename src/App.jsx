8e555564c8904253b9a2bd4d20d8e7b835fe3127
abiduzz420 <abiduzair420@gmail.com>
2017-05-24 19:24:48 +0530

    styling added

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,8 @@ class App extends React.Component {
             onChange={(event) =>
               this.setState({
               newDeadline :event.target.value})}/>
-          <Button onClick={() => this.updateDeadline()}>Submit new deadline</Button>
+          <Button
+            onClick={() => this.updateDeadline()}>Submit new deadline</Button>
         </Form>
       </div>
     );
